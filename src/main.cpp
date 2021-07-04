f0e3823833707d23378054541e6d6c3f31f64bb3
Vasilii Generalov <vasilii.generalov@gmail.com>
2021-07-04 09:58:00 +0300

    feat: render empty window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include "GL/freeglut.h"
 
-int main() {
+static void RenderCallback() {
+  glClear(GL_COLOR_BUFFER_BIT);
+  glutSwapBuffers();
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+  glutInitWindowSize(500, 200);
+  glutInitWindowPosition(0, 0);
+  glutCreateWindow("test shapes");
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+  glutDisplayFunc(&RenderCallback);
+  glutMainLoop();
   return 0;
 }
